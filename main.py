86ec2a143a8b8612a2eb982b952e39d482504fc6
fields23061998 <egorovapolina062@gmail.com>
2024-09-24 09:36:58 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+print ( 9 ** 0.5 * 5 ) # " 1st program "
+print ( 9.99 > 9.98 )   # " 2nd program "
+print ( 1000 < 1000.1 ) # " 2nd program "
+print (2 * 2 + 2 == 2 * '2'+'2') # " 3rd program "
+name = '123.456' # " 4th program "
+print (int(name[4]))
