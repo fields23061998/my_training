a369a810210c3e07a6cf04ac15c0250fcedb0378
fields23061998 <egorovapolina062@gmail.com>
2024-09-26 15:33:30 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+# name = input('my_string')
+from os import replace
+
+print('my_string'.upper())
+print('my_string'.lower())
+print('my_string'.replace('_','#'))
+name = 'my_string'
+print(name[0])
+print(name[-1])
