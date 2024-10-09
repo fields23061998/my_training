5fce86844f2486b81a876665c2feb9cf13979395
fields23061998 <egorovapolina062@gmail.com>
2024-10-09 13:45:12 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,19 @@
+my_dict = {'Vasya': 1995, 'Polya': 1998, 'Max': 2021}
+print(my_dict)
+print(my_dict.get('Vasya'))
+print(my_dict.get('Elena'))
+print(my_dict.get('Polya'))
+my_dict.update(({'Tanya': 1970, 'Sasha': 1979}))
+print((my_dict))
+del my_dict['Sasha']
+print((my_dict))
+
+my_set = {1, 2, 3, 4, 5, 5, 4, 1}
+print(my_set)
+my_set = {1, 2, 3, 4, 5, 5, 4, 1, 'kiwi',True,(1, 2, 3)}
+print(my_set)
+list_= [3, 4, 5, 5,]
+print(list_)
+print(list_.remove(3))
+print(list_)
+
