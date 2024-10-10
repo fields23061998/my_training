43ea540a5d6e9abf4cc95c49a61dad3ed43fea8f
fields23061998 <egorovapolina062@gmail.com>
2024-10-10 12:48:49 +0300

    Add files via upload

---
+++ practical task.py
@@ -0,0 +1,28 @@
+students= {'Aaron': (5, 3, 3, 5, 4), 'Bilbo': (2, 2, 2, 3), 'Johnny': (4, 5, 5, 2), 'Steve': (5, 5, 5, 4, 5), 'Khendrik': (4, 4, 3)}
+print(students)
+name='Aaron'
+print(name)
+numbers = (5, 3, 3, 5, 4) # 'Aaron'
+average = sum(numbers) / len(numbers)
+print(average)
+name='Bilbo'
+print(name)
+numbers = (2, 2, 2, 3) # 'Bilbo'
+average = sum(numbers) / len(numbers)
+print(average)
+name='Johnny'
+print(name)
+numbers = (4, 5, 5, 2) # ''Johnny''
+average = sum(numbers) / len(numbers)
+print(average)
+name='Steve'
+print(name)
+numbers = (5, 5, 5, 4, 5) # ''Steve''
+average = sum(numbers) / len(numbers)
+print(average)
+name='Khendrik'
+print(name)
+numbers = (4, 4, 3) # 'Khendrik'
+average = sum(numbers) / len(numbers)
+print(average)
+
